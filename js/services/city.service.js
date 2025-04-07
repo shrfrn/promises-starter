0e68b2bf761109dcadd2582409c05573a10f23f1
shrfrn <shrfrn@gmail.com>
2025-04-07 12:26:25 +0300

    Modify city service to work with mrjson.com

--- js/services/city.service.js
+++ js/services/city.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const STORAGE_KEY_CITIES = 'citiesDB'
-const url = 'http://www.filltext.com/?rows=10&name={city}&population={numberRange|1000,7000}'
+const url = 'http://mrjson.com/api?rows=10&name={city}&population={number|1000,7000}'
 
 function getCities() {
     // Open APIs will sometimes block you if too many requests are being sent
