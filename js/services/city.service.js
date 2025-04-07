7a3552f2e7047389e6026508f2faa71bf3fb952d
shrfrn <shrfrn@gmail.com>
2025-04-07 12:46:41 +0300

    Fix url

--- js/services/city.service.js
+++ js/services/city.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const STORAGE_KEY_CITIES = 'citiesDB'
-const url = 'http://mrjson.com/api?rows=10&name={city}&population={number|1000,7000}'
+const url = 'https://mrjson.com/api?rows=10&name={city}&population={number|1000,7000}'
 
 function getCities() {
     // Open APIs will sometimes block you if too many requests are being sent
